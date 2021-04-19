64cd1fb0030bc6a0ceae075b47ee99835f0366db
taewan <twk5196@gmail.com>
2021-04-20 01:37:47 +0900

    vnjs clock

--- assingment2/이론/index.js
+++ assingment2/이론/index.js
@@ -5,13 +5,7 @@ document.title = "my world";
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const CURRENT_CLASS = title.className;
-    if(CURRENT_CLASS !==CLICKED_CLASS){
-        title.className = CLICKED_CLASS;
-    }
-    else{
-        title.className = "";
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
